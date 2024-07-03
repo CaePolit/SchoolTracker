e1193e52fd9d1d0347318b3428eec70d9bd152fe
CaePolit <caepolit@gmail.com>
2024-07-03 16:58:33 +0200

    integrations des fichiers logs

--- SchoolTest/CourseActionTest.cs
+++ SchoolTest/CourseActionTest.cs
@@ -3,11 +3,11 @@ namespace SchoolTest
     using Xunit;
     public class CourseActionTest
     {
-        private readonly CourseAction _courseAction;
+        //private readonly CourseAction _courseAction;
 
         public CourseActionTest() 
         {
-            _courseAction = new CourseAction();
+            //_courseAction = new CourseAction();
         }
         [Fact]
         public void Add_Courses_ReturnTrue()
